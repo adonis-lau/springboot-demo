c402f35571a5c0eeabd41ada4ee1aa7745e9d918
Adonis <adonis.lau.work@gmail.com>
2017-09-04 17:17:58 +0800

    fix the restful demo bug

--- springboot-restful/src/main/java/bid/adonis/lau/service/impl/CityServiceImpl.java
+++ springboot-restful/src/main/java/bid/adonis/lau/service/impl/CityServiceImpl.java
@@ -5,6 +5,7 @@ import bid.adonis.lau.entity.City;
 import bid.adonis.lau.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import java.util.List;
  * @Date Created in 2017/9/4 16:33
  */
 @Service
+@Transactional
 public class CityServiceImpl implements CityService {
 
     @Autowired
